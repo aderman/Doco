23b0a08d5489560504323ed419655dce19ade77a
sinanakyazici <sinan_akyazici@hotmail.com>
2012-06-03 20:46:40 +0300

    - Regions and Comments has added.
    - The set method of properties in all classes has changed as private.
    - Version class has updated.
    - Validation methods has added into classes.
    - The all classes has inherited from AbstractValidator to use validation.

--- Docum.Lib/Models/DocoUser.cs
+++ Docum.Lib/Models/DocoUser.cs
@@ -1,167 +1,504 @@
-﻿using System;
-using System.Collections.Generic;
-using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
-
-namespace Docum.Lib.Models
+﻿namespace Docum.Lib.Models
 {
+    #region Using
+
+    using System;
+    using System.Collections.Generic;
+
+    using FluentValidation;
+    using FluentValidation.Results;
+
+    using MongoDB.Bson;
+    using MongoDB.Bson.Serialization.Attributes;
+
+    #endregion
 
     #region DocoUser
 
-    public class DocoUser
+    /// <summary>
+    /// DocoUser is docouser table in database. It stores the users.
+    /// </summary>
+    public class DocoUser : AbstractValidator<DocoUser>
     {
-        [BsonId]
-        public ObjectId UserId { get; set; }
+        #region Ctor
 
-        [BsonRequired]
-        public string UserName { get; set; }
-
-        public string Name { get; set; }
-
-        public string Surname { get; set; }
-
-        public string Email { get; set; }
-
-        public bool IsDeleted { get; set; }
-
-        public DocoFolder UserFolder { get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocoUser"/> class. 
+        /// This creates root folder as default. Its name is 'Root'.
+        /// </summary>
         public DocoUser()
         {
-
-            UserFolder = new DocoFolder {FolderName = "Root"};
+            this.UserFolder = new DocoFolder { FolderName = "Root" };
         }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets UserId. This is Primary Key as Hash.
+        /// </summary>
+        [BsonId]
+        public ObjectId UserId { get; private set; }
+
+        /// <summary>
+        /// Gets or sets UserName.  The user's username. This cannot be empty.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets Name. The user's name.  This cannot be empty.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets Surname. The user's surname.  This cannot be empty.
+        /// </summary>
+        public string Surname { get; set; }
+
+        /// <summary>
+        /// Gets or sets Email. The user's email address. This must have a valid email address. 
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether IsDeleted.
+        /// </summary>
+        public bool IsDeleted { get; set; }
+
+        /// <summary>
+        /// Gets or sets UserFolder. This is user's root folder.
+        /// </summary>
+        public DocoFolder UserFolder { get; set; }
+
+        #endregion
+
+        #region Validation
+
+        /// <summary>
+        /// The validations of user object.
+        /// </summary>
+        /// <returns>
+        /// If user object is validated, it returns true.
+        /// </returns>
+        public ValidationResult Validation()
+        {
+            RuleFor(x => x.UserName).NotEmpty();
+            RuleFor(x => x.Name).NotEmpty();
+            RuleFor(x => x.Surname).NotEmpty();
+            RuleFor(x => x.Email).EmailAddress();
+            return this.Validate(this);
+        }
+
+        #endregion
     }
 
     #endregion
 
-    public class DocoDocument : HistoricalObject
+    #region DocoDocument
+
+    /// <summary>
+    /// This depends on the user's folders. It stores the user's documents.
+    /// </summary>
+    public class DocoDocument : HistoricalObject<DocoDocument>
     {
-        [BsonId]
-        public ObjectId DocId { get; set; }
+        #region Ctor
 
-        public DocType DType { get; set; }
-
-        public Version DocVersion { get; set; }
-
-        public enum DocType
-        {
-            Word
-        }
-
-        public bool IsPublic { get; set; }
-
-        public bool IsCurrent { get; set; }
-
-        public List<DocoAccess> EnabledUsers { get; set; }
-
-        public List<string> Keywords { get; set; }
-
-        public string Name
-        {
-            get;
-            set;
-        }
-
-        public string DocContent{ get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocoDocument"/> class.
+        /// </summary>
         public DocoDocument()
         {
-            EnabledUsers = new List<DocoAccess>();
-            Keywords = new List<string>();
+            this.DocId = ObjectId.GenerateNewId();
+            this.EnabledUsers = new List<DocoAccess>();
+            this.Keywords = new List<string>();
         }
+
+        #endregion
+
+        #region Enums
+
+        /// <summary>
+        /// It stores document types.
+        /// </summary>
+        public enum DocType
+        {
+            /// <summary>
+            /// For word documents.
+            /// </summary>
+            Word = 0
+        }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets DocId. This is Primary Key as Hash.
+        /// </summary>
+        [BsonId]
+        public ObjectId DocId { get; private set; }
+
+        /// <summary>
+        /// Gets or sets Name. This is document name or title. This cannot be empty.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets DocContent. This is document content.
+        /// </summary>
+        public string DocContent { get; set; }
+
+        /// <summary>
+        /// Gets or sets DType. This is document type. Such as 'Word' etc.
+        /// </summary>
+        public DocType DType { get; set; }
+
+        /// <summary>
+        /// Gets or sets DocVersion. This is document version.
+        /// </summary>
+        public Version DocVersion { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether IsPublic.
+        /// </summary>
+        public bool IsPublic { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether IsCurrent.
+        /// </summary>
+        public bool IsCurrent { get; set; }
+
+        /// <summary>
+        /// Gets or sets EnabledUsers. List of users who have access to documents.
+        /// </summary>
+        public List<DocoAccess> EnabledUsers { get; set; }
+
+        /// <summary>
+        /// Gets or sets Keywords. The keywords are used during search in documents.
+        /// </summary>
+        public List<string> Keywords { get; set; }
+
+        #endregion
+
+        #region Validation
+
+        /// <summary>
+        /// The validations of document object.
+        /// </summary>
+        /// <returns>
+        /// If document object is validated, it returns true.
+        /// </returns>
+        public ValidationResult Validation()
+        {
+            RuleFor(x => x.Name).NotEmpty();
+            return this.Validate(this);
+        }
+
+        #endregion
     }
 
-    public class DocoFolder : HistoricalObject
+    #endregion
+
+    #region DocoFolder
+
+    /// <summary>
+    /// This depends on the users. It stores the user's folders.
+    /// </summary>
+    public class DocoFolder : HistoricalObject<DocoFolder>
     {
-        [BsonId]
-        public ObjectId FolderId { get; set; }
+        #region Ctor
 
-        public string FolderName { get; set; }
-
-        public string OwnerId { get; set; }
-
-        public List<DocoDocument> ListOfDocuments { get; set; }
-
-        public List<DocoFolder> Folders { get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DocoFolder"/> class.
+        /// </summary>
         public DocoFolder()
         {
-            Folders = new List<DocoFolder>();
-
-            ListOfDocuments = new List<DocoDocument>();
+            this.FolderId = ObjectId.GenerateNewId();
+            this.Folders = new List<DocoFolder>();
+            this.ListOfDocuments = new List<DocoDocument>();
         }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets FolderId. This is Primary Key as Hash.
+        /// </summary>
+        [BsonId]
+        public ObjectId FolderId { get; private set; }
+
+        /// <summary>
+        /// Gets or sets FolderName.This is folder's name. This cannot be empty.
+        /// </summary>
+        public string FolderName { get; set; }
+
+        /// <summary>
+        /// Gets or sets OwnerId. This is Id of folder's owner. 
+        /// </summary>
+        public string OwnerId { get; set; }
+
+        /// <summary>
+        /// Gets or sets ListOfDocuments. List of user's documents in folder.
+        /// </summary>
+        public List<DocoDocument> ListOfDocuments { get; set; }
+
+        /// <summary>
+        /// Gets or sets Folders. List of user's folders.
+        /// </summary>
+        public List<DocoFolder> Folders { get; set; }
+
+        #endregion
+
+        #region Validation
+
+        /// <summary>
+        /// The validations of folder object.
+        /// </summary>
+        /// <returns>
+        /// If folder object is validated, it returns true.
+        /// </returns>
+        public ValidationResult Validation()
+        {
+            RuleFor(x => x.FolderName).NotEmpty();
+            return this.Validate(this);
+        }
+
+        #endregion
     }
 
-    public class HistoricalObject
+    #endregion
+
+    #region HistoricalClass
+
+    /// <summary>
+    /// This class contains the common properties of the model classes.
+    /// </summary>
+    /// <typeparam name="T">
+    /// The Model Class
+    /// </typeparam>
+    public class HistoricalObject<T> : AbstractValidator<T>
+        where T : class
     {
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets CreatedAt.
+        /// </summary>
         public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets SavedAt.
+        /// </summary>
         public DateTime? SavedAt { get; set; }
+
+        /// <summary>
+        /// Gets or sets LastSavedBy.
+        /// </summary>
         public string LastSavedBy { get; set; }
+
+        #endregion
     }
 
+    #endregion
 
+    #region DocoAccess
+
+    /// <summary>
+    /// This class stores the user to who have access to the documents.
+    /// </summary>
     public class DocoAccess
     {
-        public string UserId { get; set; }
-        public string UserName { get; set; }
-        public AccessType AType { get; set; }
+        #region Enums
 
+        /// <summary>
+        /// This is access type of users to documents. e.g. read, Write etc.
+        /// </summary>
         public enum AccessType
         {
-            Read, Write
+            /// <summary>
+            /// Read Access
+            /// </summary>
+            Read = 0,
+
+            /// <summary>
+            /// Write Access
+            /// </summary>
+            Write = 1,
+
+            /// <summary>
+            /// Read and Write Access
+            /// </summary>
+            ReadAndWrite = 2
         }
+
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets or sets UserId. The user's id who will access document.
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets UserName. The user's name who will access document.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets AType. The access type of user name who will access document.
+        /// </summary>
+        public AccessType AType { get; set; }
+
+        #endregion
     }
 
+    #endregion
+
+    #region Version
+
+    /// <summary>
+    /// This class contains informations about the document version.
+    /// </summary>
     public class Version
     {
-        public int Major { get; set; }
+        #region Ctor
 
-        public int Minor { get; set; }
-
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Version"/> class.
+        /// </summary>
         public Version()
         {
             Major = 0;
             Minor = 0;
         }
 
-        public Version(int major,int minor)
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets Major. 
+        /// </summary>
+        public int Major { get; private set; }
+
+        /// <summary>
+        /// Gets Minor.
+        /// </summary>
+        public int Minor { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// This is used to increase version.
+        /// </summary>
+        /// <returns>
+        /// Returns new Version.
+        /// </returns>
+        public Version IncreaseVersion()
         {
-            Major = major;
-            Minor = minor;
+            this.Minor++;
+            if (this.Minor > 10)
+            {
+                this.Minor = 0;
+                this.Major++;
+            }
+
+            return this;
         }
 
-        public Version(Version v,bool justMinor)
-        {
-            Minor = v.Minor + 1;
-            if (!justMinor)
-                Major = v.Major + 1;
-        }
-
+        /// <summary>
+        /// This is used to covert string to version
+        /// </summary>
+        /// <returns>
+        /// Returns Version. Major.Minor.
+        /// </returns>
         public override string ToString()
         {
-            return String.Format("{0}.{1}", Major, Minor);
+            return string.Format("{0}.{1}", Major, Minor);
         }
+
+        #endregion
     }
 
+    #endregion
+
+    #region ActivityLog
+
+    /// <summary>
+    /// This contains the activities of all process. Used to store log.
+    /// </summary>
     public class ActivityLog
     {
-        [BsonId]
-        public ObjectId LogId { get; set; }
+        #region Enums
 
-        public ActivityType AType { get; set; }
-        public string UserName { get; set; }
-        public string UserId { get; set; }
-        public string LogContent { get; set; }
-        public DateTime LogTime { get; set; }
-
+        /// <summary>
+        /// This specifies the type of performed activity​​.
+        /// </summary>
         public enum ActivityType
         {
-            AddNewFolder,
-            AddNewDocument,
-            UpdateFolderName,
-            UpdateDocumentName
+            /// <summary>
+            /// Add a new folder
+            /// </summary>
+            AddNewFolder = 0,
+
+            /// <summary>
+            /// Add a new document
+            /// </summary>
+            AddNewDocument = 1,
+
+            /// <summary>
+            /// Update folder name
+            /// </summary>
+            UpdateFolderName = 2,
+
+            /// <summary>
+            /// Update document name
+            /// </summary>
+            UpdateDocumentName = 3
         }
 
+        #endregion
+
+        #region Properties
+
+        /// <summary>
+        /// Gets LogId.
+        /// </summary>
+        [BsonId]
+        public ObjectId LogId { get; private set; }
+
+        /// <summary>
+        /// Gets or sets AType. The type of performed activity.
+        /// </summary>
+        public ActivityType AType { get; set; }
+
+        /// <summary>
+        /// Gets or sets UserName. The user's name who performed the activity.
+        /// </summary>
+        public string UserName { get; set; }
+
+        /// <summary>
+        /// Gets or sets UserId. The user's name whow performed the activity.
+        /// </summary>
+        public string UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets LogContent. The content of performed activity.
+        /// </summary>
+        public string LogContent { get; set; }
+
+        /// <summary>
+        /// Gets or sets LogTime. The date of performed activity.
+        /// </summary>
+        public DateTime LogTime { get; set; }
+
+        #endregion
     }
+
+    #endregion
 }
